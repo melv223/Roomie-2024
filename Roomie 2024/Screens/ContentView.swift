7583330c7e2e72c683f513be429dcb87290fd64f
melv223 <mestudillo01@manhattan.edu>
2024-10-30 11:23:30 -0400

    updated color schemes

--- Roomie 2024/Screens/ContentView.swift
+++ Roomie 2024/Screens/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
--- Roomie 2024/Screens/HomeView.swift
+++ Roomie 2024/Screens/HomeView.swift
@@ -11,8 +11,8 @@ struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
     var body: some View {
         VStack(spacing: 20) {
-            
-                
+            Text("Home")
+                .font(.largeTitle)
             Button(action:{
                 isOnboardingViewActive = true
             }){
